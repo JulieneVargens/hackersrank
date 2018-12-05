b6633695e1589ea9811cfe9340258f531502dc8e
Juliene Varge <43733421+JulieneVargens@users.noreply.github.com>
2018-12-05 17:18:53 -0200

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -2,11 +2,11 @@ def test(n):
     resto = n % 2
     if resto != 0:
         print('Weird')
-    elif n==range(2,5):
+    elif n==range(2,6):
         print('Not Weird')
-    elif n==range(6,20):
+    elif n==range(6,21):
         print('Weird')
-    elif n>=20:
+    elif n>20:
         print('Not Weird')
 test(3)
 test(24)
